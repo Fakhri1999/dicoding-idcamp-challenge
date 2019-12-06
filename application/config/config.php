aef6280133838c79f80cc166b0dba25d41b702b8
Fakhri1999 <fakhriimaduddin19@gmail.com>
2019-12-06 18:26:19 +0700

    change config and gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -236,7 +236,7 @@ $config['log_threshold'] = 4;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = 'src/application/logs/';
+$config['log_path'] = 'application/logs/';
 
 /*
 |--------------------------------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
+# Hello World
+
+Aplikasi untuk mengikuti dicoding IDCamp challenge
+
 ## To-do list
+
 - [x] Persiapkan file-file yang dibutuhkan
+- [] Menampilkan list pantai di Malang selatan
